4df96a86e572306d3607893452918a888595d4be
Walt <wpniederer@cpp.edu>
2019-02-27 15:27:33 -0800

    changed main to work with new classes

--- src/main/java/StorageDriver.java
+++ src/main/java/StorageDriver.java
@@ -11,29 +11,9 @@ import java.nio.file.Paths;
 // Sync those changes to the s3 instance
 public class StorageDriver {
 
-
-    private static void usage() {
-        System.err.println("usage: java FileWatcher [-r] dir");
-        System.exit(-1);
-    }
-
     public static void main(String[] args) throws IOException {
-
-        // parse arguments
-        if (args.length == 0 || args.length > 2)
-            usage();
-        boolean recursive = false;
-        int dirArg = 0;
-        if (args[0].equals("-r")) {
-            if (args.length < 2)
-                usage();
-            recursive = true;
-            dirArg++;
-        }
-
-        // register directory and process its events
-        Path dir = Paths.get(args[dirArg]);
-        new FileWatcher(dir, recursive).processEvents();
+        // Watches for Directory changes, syncs changes
+        new FileWatcher(Paths.get("test_dir"), true).processEvents();
     }
 
 }
