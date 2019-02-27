9e94afe8b9a769070ac4396d386c87a872f67886
Walt <wpniederer@cpp.edu>
2019-02-27 15:25:19 -0800

    added method for listing bucket contents

--- src/main/java/S3Operations.java
+++ src/main/java/S3Operations.java
@@ -1,36 +1,66 @@
-import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.core.sync.RequestBody;
 
 public class S3Operations {
     private S3Client s3;
     private final String bucket = "storagecube-us-west-1";
-    private String filename;
+    private Path filename;
 
 
-    S3Operations(String filename) {
+    S3Operations(Path filename) {
         Region region = Region.US_WEST_1;
         s3 = S3Client.builder().region(region).build();
         this.filename = filename;
     }
 
     public void add() {
-        s3.putObject(PutObjectRequest.builder().bucket(bucket).key(filename).build(),
-                RequestBody.fromFile(new File(filename)));
+        System.out.println("Adding " + filename.toString() + " in " + bucket);
+        s3.putObject(PutObjectRequest.builder().bucket(bucket).key(filename.toString()).build(),
+                RequestBody.fromFile(filename));
     }
 
     public void delete() {
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(bucket).key(filename).build();
+        System.out.println("Deleting a single file at " + filename.toString() + " in " + bucket);
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(bucket).key(filename.toString()).build();
         s3.deleteObject(deleteObjectRequest);
     }
 
     public void update() {
+        System.out.println("Modifying " + filename.toString() + " in " + bucket);
         delete();
         add();
     }
+
+    public void list() {
+        ListObjectsV2Request listObjectsReqManual = ListObjectsV2Request.builder()
+                .bucket(bucket)
+                .maxKeys(1)
+                .build();
+
+        boolean done = false;
+        while (!done) {
+            ListObjectsV2Response listObjResponse = s3.listObjectsV2(listObjectsReqManual);
+            for (S3Object content : listObjResponse.contents()) {
+                System.out.println(content.key());
+            }
+
+            if (listObjResponse.nextContinuationToken() == null) {
+                done = true;
+            }
+
+            listObjectsReqManual = listObjectsReqManual.toBuilder()
+                    .continuationToken(listObjResponse.nextContinuationToken())
+                    .build();
+        }
+
+    }
+
+
 
 
 
