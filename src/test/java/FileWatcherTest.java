c1d17e40593f864e8922d9c7ae5e40a0bd50c987
Walt <wpniederer@cpp.edu>
2019-02-28 22:51:02 -0800

    added unit tests for FileWatcher

--- src/test/java/FileWatcherTest.java
+++ src/test/java/FileWatcherTest.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileWatcherTest {
+    private FileWatcher fileWatcher;
+    private Path parentDir = Paths.get("unit-test-dir");
+
+    @Before
+    public void setup() throws IOException {
+        fileWatcher = new FileWatcher(parentDir);
+    }
+
+    @Test
+    public void testRegisterAll() throws IOException {
+        FileWatcher testRegister = new FileWatcher();
+        testRegister.registerAll(parentDir);
+
+        if (!testRegister.dirList().containsAll(fileWatcher.dirList())) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testIsEmpty() {
+        Assert.assertFalse(fileWatcher.isEmpty());
+    }
+
+    // NOTE: These don't work, just process forever
+//    @Test
+//    public void testGetKey() throws InterruptedException{
+//        Assert.assertNotNull(fileWatcher.getKey());
+//    }
+
+//    @Test
+//    public void testKeysToDir() throws InterruptedException{
+//        Path dir = fileWatcher.keyToDir(fileWatcher.getKey());
+//        Assert.assertNotNull(dir);
+//    }
+
+
+
 }
